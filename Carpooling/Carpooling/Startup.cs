db9f73bc9f770267c1f4aeec45990cd9053da42e
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-03-04 09:33:48 -0500

    View and BannerController were created

--- Carpooling/Carpooling/Startup.cs
+++ Carpooling/Carpooling/Startup.cs
@@ -73,7 +73,7 @@ namespace Carpooling
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Banner}/{action=Index}/{id?}");
             });
         }
     }
